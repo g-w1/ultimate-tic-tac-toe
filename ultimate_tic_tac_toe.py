b6913e62644f8bef4783417b1bd3d90145fdd38e
g-w1 <58830309+g-w1@users.noreply.github.com>
2019-12-22 16:01:17 -0500

    maybe final?

--- ultimate_tic_tac_toe.py
+++ ultimate_tic_tac_toe.py
@@ -5,13 +5,17 @@ pygame.display.set_caption("Ultimate Tic-Tac-Toe")
 myfont = pygame.font.SysFont('Times New Roman', 30)
 xdisp = myfont.render('X', False, (0, 0, 0))
 odisp = myfont.render('O', False, (0, 0, 0))
-x_win = pygame.image.load("xwin.png").convert()
 o_win = pygame.image.load("owin.png").convert()
+x_win = pygame.image.load("xwin.png").convert()
+o_end = pygame.image.load("oend.png").convert()
+x_end = pygame.image.load("xend.png").convert()
 counter = "X"
 index = []
 wins = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 xcount = 0
 ocount = 0
+grandowin = False
+grandxwin = False
 def clicked(rect):
     if pygame.mouse.get_pressed()[0] and rect.collidepoint(pygame.mouse.get_pos()):
         return True
@@ -38,6 +42,8 @@ class Board(object):
         self.owin = False
         self.xwin = False
     def draw1(self):
+        global grandxwin
+        global grandowin
         for y in range(3):
             for x in range(3):
                 self.squares[y][x].draw()
@@ -49,9 +55,15 @@ class Board(object):
             win.blit(x_win,(self.x0,self.y0))
         if self.owin == True:
             win.blit(o_win,(self.x0,self.y0))
+        if grandowin == True:
+            win.blit(x_end,(0,0))
+        if grandxwin:
+            win.blit(o_end,(0,0))
     def update1(self):
         global index
         global counter
+        global grandowin
+        global grandxwin
         for y in range(3):
             for x in range(3):
                 if self.clickable == True:
@@ -114,6 +126,9 @@ class Square(object):
 screen = [[Board(0*333,0*333),Board(1*333,0*333),Board(2*333,0*333)],
 [Board(0*333,1*333),Board(1*333,1*333),Board(2*333,1*333)],
 [Board(0*333,2*333),Board(1*333,2*333),Board(2*333,2*333)]]
+screenlong = [Board(0*333,0*333),Board(1*333,0*333),Board(2*333,0*333),
+Board(0*333,1*333),Board(1*333,1*333),Board(2*333,1*333),
+Board(0*333,2*333),Board(1*333,2*333),Board(2*333,2*333)]
 win.fill((255,255,255))        
 run_game = True
 while run_game:
@@ -126,6 +141,19 @@ while run_game:
             space.draw1()
             space.check_win()
             space.clickable = False
+    for board in screenlong:
+        for test in wins:
+            xcount = 0
+            ocount = 0
+            for squares in test:
+                if screenlong[squares].xwin == True:
+                    xcount += 1
+                if screenlong[squares].owin == True:
+                    ocount +=1
+                if ocount == 3:
+                    grandowin = True
+                if xcount == 3:
+                    grandxwin = True
     if index == []:
         for row in screen:
             for space in row:
