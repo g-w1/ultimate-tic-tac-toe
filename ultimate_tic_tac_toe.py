cc4cdcfe799a50ea0770fa7ed9e9e336526924e8
g-w1 <58830309+g-w1@users.noreply.github.com>
2019-12-21 21:00:37 -0500

    got a display. nothing else

--- ultimate_tic_tac_toe.py
+++ ultimate_tic_tac_toe.py
@@ -7,9 +7,9 @@ xdisp = myfont.render('X', False, (0, 0, 0))
 odisp = myfont.render('O', False, (0, 0, 0))
 counter = "X"
 def clicked(rect):
-    if pygame.mouse.get_pressed()[0] and pygame.Rect(rect).collidepoint(pygame.mouse.get_pos()):
-        return true
-class Board:
+    if pygame.mouse.get_pressed()[0] and rect.collidepoint(pygame.mouse.get_pos()):
+        return True
+class Board(object):
     def __init__(self,x,y):
         self.rect = (x,y,333,333)
         self.x = x
@@ -26,16 +26,39 @@ class Board:
         self.squares = [[Square(self.x0,self.y0),Square(self.x1,self.y0),Square(self.x2,self.y0)],
                         [Square(self.x0,self.y1),Square(self.x1,self.y1), Square(self.x2,self.y1)],
                         [Square(self.x0,self.y2), Square(self.x1,self.y2),Square(self.x2,self.y2)]]
-class Square:
+    def draw1(self):
+        for y in self.squares:
+            for x in y:
+                x.draw()
+class Square(object):
     def __init__(self,x,y):
         self.rect = (x,y,111,111)
         self.y = y
-        self.x = X
+        self.x = x
         self.clickable = True
         self.done = False
-        self.value = ""
+        self.value = "X"
+        self.rect = pygame.Rect(self.x,self.y,111,111)
+    def draw(self):
+        '''if self.value == "X":
+            win.blit(xdisp,(self.x,self.y))
+        if self.value == "O":
+            win.blit(odisp,(self.x,self.y))'''
+        pygame.draw.rect(win,(0,0,0),(self.x,self.y,111,111))
 run_game = True
+'''screen = []
+for y in range(3):
+    for x in range(3):
+        screen.append(Board(x*333,y*333))'''
+screen = Board(0,0)
 while run_game:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run_game = False
     win.fill((255,255,255))
     pygame.display.update()
-    pass
+    '''for space in screen:
+        space.draw1()'''
+    screen.draw1()
+pygame.quit()
+print(screen)
